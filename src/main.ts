571f31db45288d8e9b17c8a8842ee288d05789fa
meranHM <mehranshahani6@gmail.com>
2025-06-12 18:30:46 +0330

    Add sliding in function for hamburger menu button for mobile view navbar

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
-import './style.css'
+import './index.css'
 
+/* Hamburger Menu sliding in function for mobile view */
+const hamburgerBtn = document.getElementById("hamburger-btn")
+const mobileNavbar = document.getElementById("mobile-navbar")
+let isMenuOpen = false
+
+hamburgerBtn?.addEventListener("click", () => {
+  if (!mobileNavbar) return
+
+  isMenuOpen = !isMenuOpen
+  mobileNavbar.classList.toggle("translate-x-0", isMenuOpen)
+  mobileNavbar.classList.toggle("translate-x-full", !isMenuOpen)
+})
